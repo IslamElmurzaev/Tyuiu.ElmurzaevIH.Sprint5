b89a4e315e7b4e17450a91f85b373293f5ae1682
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-11-19 21:44:34 +0500

    Task0

--- Tyuiu.ElmurzaevIH.Sprint5.Task0.V4/Program.cs
+++ Tyuiu.ElmurzaevIH.Sprint5.Task0.V4/Program.cs
@@ -15,6 +15,20 @@ namespace Tyuiu.ElmurzaevIH.Sprint5.Task0.V4
             int x = 2;
 
             DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Ельмурзаев И.Х. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Ельмурзаев Ислам Хаважевич | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2, результат сохранить   *");
+            Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль.              *");
+            Console.WriteLine("* Округлить до 3 знаков после запятой.                                    *");
+            Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
