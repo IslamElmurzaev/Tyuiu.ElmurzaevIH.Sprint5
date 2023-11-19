04f3a5bcd1eead8b2c000a6aac129ec6f88a4d19
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-11-19 22:18:18 +0500

    Task1

--- Tyuiu.ElmurzaevIH.Sprint5.Task1.V29/Program.cs
+++ Tyuiu.ElmurzaevIH.Sprint5.Task1.V29/Program.cs
@@ -17,13 +17,13 @@ namespace Tyuiu.ElmurzaevIH.Sprint5.Task1.V29
             Console.Title = "Спринт #5 | Выполнил: Ельмурзаев И.Х. | ИИПб-23-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
             Console.WriteLine("* Выполнил: Ельмурзаев Ислам Хаважевич | ИИПб-23-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2, результат сохранить   *");
+            Console.WriteLine("* Дана функция на [-5, 5] с шагом 1. Результат сохранить в текстовый файл *");
             Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль.              *");
             Console.WriteLine("* Округлить до 3 знаков после запятой.                                    *");
             Console.WriteLine("*                                                                         *");
